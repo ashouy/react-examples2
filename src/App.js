2d1d91b14e7c7566388d519e9ac95ad0a574ea3c
ashouy <joaopaulokad@gmail.com>
2021-05-06 22:32:13 -0300

    add description

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Example1 from "./components/example1/Example1";
 
 function App() {
-
   return (
     <div className="App">
-      <section >
-        <Example1/>
+      <section id="first-example">
+        <p>
+          In this example we learn how to add a custom modal an reuse it at any
+          component
+        </p>
+        <Example1 />
       </section>
+      <hr/>
     </div>
   );
 }
